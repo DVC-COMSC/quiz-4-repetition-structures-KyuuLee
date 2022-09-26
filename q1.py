e0f729d475d8c75e0c84770bdd76347cf449fc25
Kyu Lee <kwooler@gmail.com>
2022-09-25 21:28:37 -0700

    q1

--- q1.py
+++ q1.py
@@ -8,3 +8,14 @@
 # No need to use list
 # All input values are taken one by one separatively.
 ###
+prev_even = 0
+cnt = 0
+on = 1
+for i in range(10):
+    current = int(input())
+    if prev_even and (current % 2 == 0) and on:
+        cnt += 1
+        on = 0
+    on = 1 if current % 2 == 1 else on
+    prev_even = 1 if (current % 2 == 0) else 0
+print(cnt)
